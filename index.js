9e5a8476b9493c65637c8ae1be5771356a2e9ee9
Birdo77 <paraservicosemail@gmail.com>
2023-06-08 14:33:40 -0300

    adding bulma and ejs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.send("We are on raspeberry pi modafocar yeyyyyyyyyyyyyyyyy");
+    res.render("main");
 });
 
 app.listen("8080", () => {
