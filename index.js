c5afd7362a4a2ec2a7590f59d060bab6e43d7965
Birdo77 <paraservicosemail@gmail.com>
2023-06-10 11:07:21 -0300

    Created database and website

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const Decimal = require('decimal.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("main");
+    const users = JSON.parse(fs.readFileSync("./db.json")).data;
+    res.render("main", {users});
+});
+
+app.post("/changeVal", (req, res) => {
+    if (req.body.password == "lixoinatorUser"){
+        const users = JSON.parse(fs.readFileSync("./db.json"));
+        const index = Number(req.body.user.index);
+
+        vl = new Decimal(req.body.value);
+        credit = new Decimal(users.data[index].credit);
+        users.data[index].credit = credit.minus(vl);
+
+        fs.writeFileSync("./db.json", JSON.stringify(users));
+
+        res.sendStatus(200);
+    }else{
+        res.sendStatus(400);
+    }
+});
+
+app.post("/addVal", (req, res) => {
+    if (req.body.password == "lixoinatorUser"){
+        const users = JSON.parse(fs.readFileSync("./db.json"));
+        const index = Number(req.body.user.index);
+
+        vl = new Decimal(req.body.value);
+        credit = new Decimal(users.data[index].credit);
+        users.data[index].credit = credit.sum(vl);
+
+        fs.writeFileSync("./db.json", JSON.stringify(users));
+
+        res.sendStatus(200);
+    }else{
+        res.sendStatus(400);
+    }
+});
+
+app.get("/getUsers", (req, res)=> {
+    const users = JSON.parse(fs.readFileSync("./db.json")).data;
+    res.send(users);
 });
 
 app.listen("8080", () => {
