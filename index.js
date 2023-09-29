0a4de605965e8286be9853bbd1faf3b272ae5e78
Birdo77 <paraservicosemail@gmail.com>
2023-09-29 18:47:21 -0300

    fky

---
+++ index.js
@@ -0,0 +1,39 @@
+Derãoderãonão
+Derãonão
+Erãoderãonão
+Derãonão
+Erãoderãonão
+Olha só, ó, ó, ó
+Invadindo a França, ah, assalto de grande porte
+Tipo entrar na Casa Branca e levar tudo que nós pode
+No topo do Louvre, descendo de corda memo'
+Hoje nós vai levar tudo e tamo' pouco se fudendo, ha
+Minha vó quer jantar debaixo da última ceia
+Então vou levar pra ela de presente na sexta
+E se ela fizer uma desfeita, mano, não vai ter problema
+Levo mais uma dezena que é só pra ter certeza
+Da Vinci code, bitch, mano, nós tá invisível
+Incrível na noite o kit, mano, é Missão Impossível
+Então aumenta a segurança, então corre e fecha as tranca
+Que se os moleque chegar, eles rapa ca' tua banca
+Da Vinci code, bitch, mano nós tá invisível
+Incrível na noite o kit, mano, é Missão Impossível
+Então aumenta a segurança, então corre e fecha as tranca
+Que se os moleque chegar, eles rapa ca' tua banca e toma tudo
+A segurança tá fraca, nós entrou e levo tudo
+As trava que protegia, nós desfez com um canudo
+Desnudo, o Homem Vitruviano, nós carregamo'
+E levamo' pra nossa vó que agora tá decifrando
+Da Vinci code, bitch
+Tá fudido Da Vinci, pegamo' teu autorretrato
+Vamo desvendar o barato
+A equipe cheia dos kit, pronta pra desvendar o fato
+E ter fama pelas street, agora nós fica rich, bitch
+Da Vinci code, bitch, mano, nós ta invisível
+Incrível na noite o kit, mano, é Missão Impossível
+Então aumenta a segurança, então corre e fecha as tranca
+Que se os moleque chegar, eles rapa ca' tua banca
+Da Vinci code, bitch, mano, nós tá invisível
+Incrível na noite o kit, mano, é Missão Impossível
+Então aumenta a segurança, então corre e fecha as tranca
+Que se os moleque chegar, eles rapa ca' tua banca e toma tudo
